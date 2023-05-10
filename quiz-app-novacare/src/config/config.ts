e86bd32c653f60edec889894852b20248a296bc6
Johannes Erdahl Andresen <johannes.erdahl.andresen@gmail.com>
2023-05-10 23:00:48 +0200

    add some explanation to the config

--- quiz-app-novacare/src/config/config.ts
+++ quiz-app-novacare/src/config/config.ts
@@ -1,6 +1,7 @@
 import { createClient } from "contentful";
 
 //Disse verdiene bør ikke ligge her, men fordi det er et test-prosjekt lar jeg de ligge
+//Dersom jeg skulle brukt disse verdiene i en react app, ville jeg bruke process.env.ACCESS_TOKEN osc.. Dette er av sikkerhetsmessige årsaker.
 const ACCESS_TOKEN = "nlYde6hGa_MS0B0Ip1pMMQFGs0icI_ntabFjMhrjaUw";
 
 const SPACE = "q0fnx0gj00da";
